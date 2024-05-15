287d0bfd510e5e0bb9871a9f8305f00a91623d58
natt <natii.v101@gmail.cocmgit config --global user.email natii.v101@gmail.com>
2024-05-15 19:54:41 -0300

    Ejercicio 29

---
+++ Ejercicio 26 repaso.py
@@ -0,0 +1,10 @@
+#Ejercicio 26
+#Entradas
+n1: ('Ingrese su numero: ')
+n2: ('Ingrese su numero: ')
+#Proceso
+if n1<=n2:
+    print(n1 es el menor o igual)
+    print(
+else:
+    print(n2 es mayor)
---
+++ Ejercicio 29.py
@@ -0,0 +1,17 @@
+#Ejercicio 29
+#Entradas
+fondo:float(input('Ingrese medida de fondo: '))
+frente:float(input('Ingrese medida de frente: '))
+terreno: (rectangular or cuadrado)
+#Proceso
+p_rec= (2*frente+2*fondo)
+s_rec= (fondo*frente)
+if p_cuadrado= (4*frente)
+   s_cuadrado= (frente*fondo)
+   print(El terreno es cuadrado)
+else:
+    print(terreno)
+    
+
+if frente == fondo
+
